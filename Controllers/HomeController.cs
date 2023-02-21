cfee70605a37542214fdcaaf1741d343b9d03d9a
AlexRasch <46262688+AlexRasch@users.noreply.github.com>
2023-02-21 17:28:19 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net;
+using WebApplication1.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Weather")]
+        [ResponseCache(Duration = 360, Location = ResponseCacheLocation.Any)]
+        public string Weather()
+        {
+            string strApiKey = "244871bd48c098405479a6e7e4971222"; // Should be stored more secure
+            int iCnt = 40;
+            //string strCityId = "2701680"; // Karlstad
+            string strLon = "13.50357";
+            string strLat = "59.379299";
+
+            string strUrl = $"https://api.openweathermap.org/data/2.5/forecast?lat={strLat}&lon={strLon}&cnt={iCnt}&units=metric&lang=SE&appid={strApiKey}";
+#if DEBUG
+            Debug.WriteLine($"Url: {strUrl}");
+#endif
+
+            // HttpClient is intended to be instantiated once per application, rather than per-use.
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = client.GetAsync(strUrl).Result)
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        return content.ReadAsStringAsync().Result;
+                    }
+                }
+            }
+        }
+
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/weather.js
@@ -0,0 +1,64 @@
+﻿const strEndpoint = "https://localhost:7264/weather";
+let strResponse;
+
+let elemTable;
+
+window.onload = (event) => {
+    console.log("Page is fully loaded");
+
+    elemTable = document.getElementById('tblWeather');
+
+    // Get response from backend
+    fetch(strEndpoint)
+        .then(res => res.json())
+        .then((out) => {
+            strResponse = out;
+            fnDisplay();
+        }).catch(err => console.error(err));
+};
+
+
+function fnDisplay() {
+
+    // Clear tbl
+    //var tableHeaderRowCount = 1;
+    //var rowCount = elemTable.rows.length;
+    //
+    //for (var i = tableHeaderRowCount; i < rowCount; i++) {
+    //    elemTable.deleteRow(tableHeaderRowCount);
+    //}
+
+
+    // Da Loop
+    var iCurrent = 0;
+    var strOldDate;
+
+    for (var i = 0; i < strResponse.list.length; i++) {
+
+        if (i % 4 == 0) {
+            console.log("HitMe");
+        }
+
+        if (strResponse.list[i].dt_txt.includes("12:00") || strResponse.list[i].dt_txt.includes("18:00")) {
+            iCurrent = iCurrent + 1;
+            var row = elemTable.insertRow(iCurrent);
+            row.insertCell(0).innerHTML = strResponse.list[i].dt_txt;
+
+            row.insertCell(1).innerHTML = '';
+            row.insertCell(2).innerHTML = '';
+            row.insertCell(3).innerHTML = '';
+            row.insertCell(4).innerHTML = '';
+
+            row.insertCell(5).innerHTML = strResponse.list[i].main.temp_min + ' / ' + strResponse.list[i].main.temp_max;
+            row.insertCell(6).innerHTML = GetIconUrl(strResponse.list[i].weather[0].icon, strResponse.list[i].weather[0].description);
+            console.log(strResponse.list[i]);
+        }
+
+
+    }
+}
+
+
+function GetIconUrl(strIcon, strTitle) {
+    return '<img src="https://openweathermap.org/img/wn/' + strIcon + '.png" title="' + strTitle + '"/>';
+}
